595b51fc06dbc9e4d8d5d1dbdd1e9ae9551d0286
Lexsser <danielprosito333@gmail.com>
2026-04-22 13:28:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php include './plantillas/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
+    <link rel="stylesheet" href="./CSS/estilos.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
