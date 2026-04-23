5e35a24c06d2fdecffe70a339ab64e000ef2acc5
p73franky <hugofrancomon@gmail.com>
2026-04-23 13:15:59 +0200

    cambios para merge

--- plantillas/header.php
+++ plantillas/header.php
@@ -1,54 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">  
+<html lang="en">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/estilossss.css">
+    <link rel="stylesheet" href="./CSS/estiloss.css">
     <title>Ola</title>
 </head>
 <body>
 <!-- Checkbox principal: controla apertura del menú en móvil -->
-<input type="checkbox" id="interruptor-menu" class="interruptor-menu">
 
-<header class="cabecera">
+<header>
+    <input type="checkbox" id="interruptor-menu" class="interruptor-menu">
 
     <div>
         <a href="index.php">Sincronizador de Bases de Datos</a>
     </div>
 
+<!-- Sustituir los span por icono-->
     <label for="interruptor-menu">
         <span></span>
         <span></span>
         <span></span>
     </label>
 
+    <nav>
+        <ul>
+
+            <li><a href="index.php">Inicio</a></li>
+
+            <li>
+                <input type="checkbox" id="submenu-conexiones">
+                <label for="submenu-conexiones">Conexiones ▾</label>
+                <ul>
+                    <li><a href="#">Nueva conexión</a></li>
+                    <li><a href="#">Mis conexiones</a></li>
+                    <li><a href="#">Probar conexión</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <input type="checkbox" id="submenu-sync">
+                <label for="submenu-sync">Sincronización ▾</label>
+                <ul>
+                    <li><a href="#">Nueva tarea</a></li>
+                    <li><a href="#">Tareas activas</a></li>
+                    <li><a href="#">Historial</a></li>
+                    <li><a href="#">Configuración</a></li>
+                </ul>
+            </li>
+
+            <li><a href="#">Registros</a></li>
+
+        </ul>
+    </nav>
+
 </header>
-
-<nav>
-    <ul>
-
-        <li><a href="index.php">Inicio</a></li>
-
-        <li>
-            <input type="checkbox" id="submenu-conexiones">
-            <label for="submenu-conexiones">Conexiones ▾</label>
-            <ul>
-                <li><a href="conexiones/nueva.php">Nueva conexión</a></li>
-                <li><a href="conexiones/lista.php">Mis conexiones</a></li>
-                <li><a href="conexiones/probar.php">Probar conexión</a></li>
-            </ul>
-        </li>
-
-        <li>
-            <input type="checkbox" id="submenu-sync">
-            <label for="submenu-sync">Sincronización ▾</label>
-            <ul>
-                <li><a href="sync/nueva.php">Nueva tarea</a></li>
-                <li><a href="sync/activas.php">Tareas activas</a></li>
-                <li><a href="sync/historial.php">Historial</a></li>
-                <li><a href="sync/configuracion.php">Configuración</a></li>
-            </ul>
-        </li>
-
-        <li><a href="registros.php">Registros</a></li>
-
-    </ul>
-</nav>
